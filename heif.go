bc0fee34ecf6b7cb5fd1dbd2e1a6d090f40f9197
Kenneth Shaw <kenshaw@gmail.com>
2024-02-07 14:36:44 +0700

    Adding -static to ldflags

--- heif.go
+++ heif.go
@@ -9,7 +9,7 @@ package heif
 
 #cgo CFLAGS: -I${SRCDIR}
 
-#cgo LDFLAGS: -lheif -lde265 -lx265 -laom -lwebp -lwebpdecoder -lwebpdemux -lwebpmux -lsharpyuv -lSvtAv1Enc -lz -lm -lstdc++
+#cgo LDFLAGS: -static -lheif -lde265 -lx265 -laom -lwebp -lwebpdecoder -lwebpdemux -lwebpmux -lsharpyuv -lSvtAv1Enc -lz -lm -lstdc++
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/libheif/darwin_amd64
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/libheif/darwin_arm64
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libheif/linux_amd64
